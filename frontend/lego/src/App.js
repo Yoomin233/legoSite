276726bf5499c9b4a1fd358016878f399fda8fc8
Yuemin.Hu <yuemin.hu@baidao.com>
2017-06-10 16:07:01 +0800

    before ejecting

--- frontend/lego/src/App.js
+++ frontend/lego/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
-import './App.css';
-import './config'
+import appCss from './stylesheets/App.css';
+
+import config from './config'
+import axios from 'axios'
 
 class App extends Component {
+  componentDidMount () {
+    console.log('mounted!')
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className={appCss['App-header']}>
+          <h2>Lego!</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
