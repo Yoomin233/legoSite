956b88b4d68b81c5b0373f287654f183b2034c0e
Yuemin.Hu <yuemin.hu@baidao.com>
2017-09-19 10:11:17 +0800

    minor changes

--- frontend/lego/src/config/index.js
+++ frontend/lego/src/config/index.js
@@ -1,10 +1,10 @@
 const config = {
   development: {
-    rootURL: 'http://47.94.196.246:3080',
+    rootURL: 'http://www.yoominhu.site',
     rootPath: '/'
   },
   production: {
-    rootURL: 'http://47.94.196.246:3080',
+    rootURL: 'http://www.yoominhu.site',
     rootPath: '/lego/'
   }
 }
